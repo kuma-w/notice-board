5a4326ac4ba5421514cface9dbcd57fe193039c3
woong <woong@autoa2z.co.kr>
2024-08-23 18:33:01 +0900

    feat : [backend] 게시글 디테일 라우팅 추가

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -28,7 +28,6 @@ app.get("/users", async (req, res) => {
   try {
     // 쿼리 실행
     const result = await pool.query("SELECT * FROM users");
-    console.log(result);
     res.json(result.rows); // 쿼리 결과를 JSON 형식으로 응답
   } catch (err) {
     console.error(err);
@@ -41,8 +40,28 @@ app.get("/board", async (req, res) => {
   try {
     // 쿼리 실행
     const result = await pool.query("SELECT * FROM board");
-    console.log(result);
     res.json(result.rows); // 쿼리 결과를 JSON 형식으로 응답
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Database query error" });
+  }
+});
+
+app.get("/board/write/:id", async (req, res) => {
+  const { id } = req.params; // URL에서 id를 추출
+
+  try {
+    // 인덱스(id)가 일치하는 로우 하나 가져오기
+    const result = await pool.query("SELECT * FROM board WHERE idx = $1", [id]);
+
+    // 결과가 존재하는지 확인
+    if (result.rows.length === 0) {
+      return res
+        .status(404)
+        .json({ error: "No data found for the provided ID" });
+    }
+
+    res.json(result.rows[0]); // 쿼리 결과를 JSON 형식으로 응답
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Database query error" });
